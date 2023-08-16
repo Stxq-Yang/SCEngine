3815e6d459d713cc74f3efd711a61b1f844f49db
Stxq-Yang <84013188+Stxq-Yang@users.noreply.github.com>
2023-08-16 17:06:57 +0800

    Create Event.cpp

---
+++ Event.cpp
@@ -0,0 +1,18 @@
+#include "Event.h"
+
+int Event::firstlevel = 5;
+
+Event::Event(long long time) : dtime(time) {
+}
+
+int Event::getFirstLevel() {
+    return firstlevel;
+}
+
+long long Event::getDetachTime() {
+    return dtime;
+}
+
+std::string Event::getClassName() {
+    return EventClassName;
+}
