153dc5d5bf0c1da76b9f59d71be360de24482470
Stxq-Yang <84013188+Stxq-Yang@users.noreply.github.com>
2023-08-16 18:08:38 +0800

    Update TimerEvent.cpp

--- TimerEvent.cpp
+++ TimerEvent.cpp
@@ -1,10 +1,5 @@
-#ifndef TIMEREVENT_CPP
-#define TIMEREVENT_CPP
-#include "Timer.cpp"
-#include "Event.cpp"
 #include "TimerEvent.h"
 namespace SCE  {
   int Event::firstlevel = 0;
   std::string Event::EventClassName = "TimerEvent";
 }
-#endif
