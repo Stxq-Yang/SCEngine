3a368c3aab0d82292757c67e357a7a247e42cae0
Stxq-Yang <84013188+Stxq-Yang@users.noreply.github.com>
2023-08-16 17:11:58 +0800

    Update EventReceiver.h

--- EventReceiver.h
+++ EventReceiver.h
@@ -23,5 +23,19 @@ public:
 private:
     std::map<std::string, std::vector<std::function<void(Event*)>>> registeredEvent;
 };
+struct eventQueueOrder {
+    bool operator()(const Event* event1, const Event* event2) {
+        return event1->getFirstLevel() < event2->getFirstLevel();
+    }
+};
+
+std::priority_queue<Event*, std::vector<Event*>, eventQueueOrder> EventQueue;
+std::vector<EventReceiver*> EventBus;
+
+void processEvent();
+
+void sendEvent(Event* event);
+
+void processAllEvent();
 };
 #endif // EVENTRECEIVER_H
