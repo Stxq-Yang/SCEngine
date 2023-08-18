d9bd52d29947a1e147d1e999b25c999c78bf358e
Stxq-Yang <84013188+Stxq-Yang@users.noreply.github.com>
2023-08-18 09:47:08 +0800

    Update PaintEvent.cpp

--- PaintEvent.cpp
+++ PaintEvent.cpp
@@ -2,4 +2,19 @@
 namespace SCE  {
   int PaintEvent::firstlevel = 3;
   std::string PaintEvent::EventClassName = "PaintEvent";
+  #ifdef _WIN32
+        HWND PaintEvent::getWindow(){
+          return window;
+        }
+        void PaintEvent::setWindow(HWND window){
+          this->window=window;
+        }
+   #elif __linux__
+        HWND PaintEvent::getWindow(){
+          return window;
+        }
+        void PaintEvent::setWindow(Window window){
+          this->window=window;
+        }
+   #endif
 }
