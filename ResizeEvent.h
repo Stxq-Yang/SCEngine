559a266c139c354009f6181638fdd6f30117b041
Stxq-Yang <84013188+Stxq-Yang@users.noreply.github.com>
2023-08-18 10:39:34 +0800

    Update ResizeEvent.h

--- ResizeEvent.h
+++ ResizeEvent.h
@@ -1,4 +1,9 @@
 #include "Event.h"
+#ifdef _WIN32
+     #include <windows.h>
+#elif __linux__
+     #include <X11/Xlib.h>
+#endif
 namespace SCE{
 class ResizeEvent:public Event{
 public:
@@ -6,7 +11,19 @@ public:
   void setSize(int width,int high);
   int getWidth();
   int getHigh();
+   #ifdef _WIN32
+        HWND getWindow();
+        void setWindow(HWND window);
+   #elif __linux__
+        HWND getWindow();
+        void setWindow(Window window);
+   #endif
 private:
   int width,high;
+   #ifdef _WIN32
+        HWND window;
+   #elif __linux__
+        Window window;
+   #endif
 }
 }
