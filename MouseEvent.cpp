d5ca488f5d87a95ca62935d12369be166b263dfa
Stxq-Yang <84013188+Stxq-Yang@users.noreply.github.com>
2023-08-18 12:18:02 +0800

    Update MouseEvent.cpp

--- MouseEvent.cpp
+++ MouseEvent.cpp
@@ -1,11 +1,35 @@
 #include "MouseEvent.h"
 namespace SCE{
   int MouseEvent::firstlevel = 5;
-  std::string TimerEvent::EventClassName = "MouseEvent";
-  void KeyEvent::setEventType(MouseEventType type){
+  std::string MouseEvent::EventClassName = "MouseEvent";
+  void MouseEvent::setEventType(MouseEventType type){
     eventType=type;
   }
-  MouseEventType KeyEvent::getEventType(){
+  MouseEventType MouseEvent::getEventType(){
     return eventType;
+  }
+  void MouseEvent::setScrollParam(int param){
+    this->param=param;
+  }
+  int MouseEvent::getScrollParam(){
+    return param;
+  }
+  void MouseEvent::setPosition(int x,int y){
+    this->lastx=this->x;
+    this-.lasty=this->y;
+    this->x=x;
+    this->y=y;
+  }
+  int MouseEvent::getX(){
+    return x;
+  }
+  int MouseEvent::getY(){
+    return y;
+  }
+  int MouseEvent::getlastX(){
+    return lastx;
+  }
+  int MouseEvent::getlastY(){
+    return lasty;
   }
 }
