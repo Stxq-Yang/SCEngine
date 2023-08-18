bb62f1f33265d25ff0960bf241ef8231b7e148c5
Stxq-Yang <84013188+Stxq-Yang@users.noreply.github.com>
2023-08-18 11:03:21 +0800

    Update KeyEvent.cpp

--- KeyEvent.cpp
+++ KeyEvent.cpp
@@ -2,9 +2,16 @@
 namespace SCE{
   int KeyEvent::firstlevel = 5;
   std::string TimerEvent::EventClassName = "KeyEvent";
-  void KeyEvent::setEventType(KeyEventType type);
-  KeyEventType KeyEvent::getEventType();
-  void KeyEvent::setKayCode(SCKey type);
-  SCKey KeyEvent::getKeyCodee();
-
+  void KeyEvent::setEventType(KeyEventType type){
+    eventType=type;
+  }
+  KeyEventType KeyEvent::getEventType(){
+    return eventType;
+  }
+  void KeyEvent::setKayCode(SCKey code){
+    keyCode=code
+  }
+  SCKey KeyEvent::getKeyCodee(){
+    return keyCode;
+  }
 }
