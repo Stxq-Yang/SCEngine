f18b7a66a20d8e0dce2c27942221cfc52cbdce3d
Stxq-Yang <84013188+Stxq-Yang@users.noreply.github.com>
2023-08-18 09:43:47 +0800

    Update PaintEvent.h

--- PaintEvent.h
+++ PaintEvent.h
@@ -1,7 +1,25 @@
 #include "Event.h"
+#ifdef _WIN32
+     #include <windows.h>
+#elif __linux__
+     #include <X11/Xlib.h>
+#endif
 namespace SCE{
 class PaintEvent :public Event{
 public:
      PaintEvent(){}
+     #ifdef _WIN32
+          HWND getWindow();
+          void setWindow(HWND window);
+     #elif __linux__
+          HWND getWindow();
+          void setWindow(Window window);
+     #endif
+private
+     #ifdef _WIN32
+          HWND window;
+     #elif __linux__
+          Window window;
+     #endif
 }
 }
